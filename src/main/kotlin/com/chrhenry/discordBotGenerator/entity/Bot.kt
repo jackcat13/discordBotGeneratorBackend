8352bdbfa3737b64009a0220682db4a161b87013
HENRY Christophe <christophe.henry2@soprabanking.com>
2021-07-22 17:27:07 +0200

    feat: add id to Bot entity

--- src/main/kotlin/com/chrhenry/discordBotGenerator/entity/Bot.kt
+++ src/main/kotlin/com/chrhenry/discordBotGenerator/entity/Bot.kt
@@ -1,6 +1,7 @@
 package com.chrhenry.discordBotGenerator.entity
 
+import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 
 @Document
-data class Bot(val id: String, val description: String, val user: User)
+data class Bot(@Id val id: String, val description: String, val user: User)
