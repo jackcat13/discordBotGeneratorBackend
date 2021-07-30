ab18d932e61f2d156f0aa3066d9cd3cd9761aceb
HENRY Christophe <christophe.henry2@soprabanking.com>
2021-07-30 19:19:47 +0200

    feat: Avoid duplicated jda processes

--- src/main/kotlin/com/chrhenry/discordBotGenerator/service/JdaStarterService.kt
+++ src/main/kotlin/com/chrhenry/discordBotGenerator/service/JdaStarterService.kt
@@ -4,16 +4,24 @@ import com.chrhenry.discordBotGenerator.entity.Bot
 import com.chrhenry.discordBotGenerator.jda.BotStarter
 import net.dv8tion.jda.api.JDA
 import net.dv8tion.jda.api.JDA.Status.CONNECTED
+import net.dv8tion.jda.internal.utils.tuple.MutablePair
 import org.springframework.stereotype.Service
 
 @Service
 class JdaStarterService(private val botStarter: BotStarter) {
 
-    private val botProcesses = mutableSetOf<Pair<String, JDA>>()
+    private val botProcesses = mutableSetOf<MutablePair<String, JDA>>()
 
     fun startBot(bot: Bot) {
-        botProcesses.add(bot.id to BotStarter.run(bot.configuration))
+        if (!botProcesses.any{ it.left == bot.id }) addProcess(bot)
+        else if (botProcesses.any{ it.left == bot.id && it.right.status != CONNECTED }) restartProcess(bot)
     }
 
-    fun getBotServiceStatus(id: String): Boolean = botProcesses.any { it.first == id && it.second.status == CONNECTED }
+    private fun addProcess(bot: Bot){ botProcesses.add(bot.id mutableTo BotStarter.run(bot.configuration)) }
+
+    private fun restartProcess(bot: Bot){ botProcesses.first { it.left == bot.id }.right = BotStarter.run(bot.configuration) }
+
+    fun getBotServiceStatus(id: String): Boolean = botProcesses.any { it.left == id && it.right.status == CONNECTED }
 }
+
+private infix fun String.mutableTo(jda: JDA): MutablePair<String, JDA> = MutablePair(this, jda)
